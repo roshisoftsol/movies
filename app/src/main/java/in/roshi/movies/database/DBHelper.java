44e52693f9fa917a7e5380d2a40ec3587dc6ba00
Nishant Bhatia <nishant@roshi.in>
2017-07-04 15:39:17 +0530

    Reset db version to 1

--- app/src/main/java/in/roshi/movies/database/DBHelper.java
+++ app/src/main/java/in/roshi/movies/database/DBHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "mymovie.db";
-    private static final int    DATABASE_VER  = 2;
+    private static final int    DATABASE_VER  = 1;
 
     public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VER);
